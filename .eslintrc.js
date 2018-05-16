cae318c5c3aa80fe39a154b10da03c1cb6803034
jon <jon_bullen@live.com>
2018-05-16 12:38:35 -0700

    Local template changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,11 @@ module.exports = {
     },
     "extends": "eslint:recommended",
     "parserOptions": {
-        "sourceType": "module"
+        "sourceType": "module",
+        "ecmaVersion": 6,
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true
+        }
     },
     "plugins": [
         "html"
